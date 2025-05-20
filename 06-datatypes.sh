7358768e22d2fbb64e362d29f476fafc03384ca6
anjuma <anjuma.sk11@gmail.com>
2025-05-20 10:01:56 +0530

    datatypes

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -3,7 +3,7 @@ Num2=$2
 
 SUM=$(( $Num1 + $Num2 ))
 $SUM
-echo "sum of $Num1 and $Num2 is $sum" 
+echo "sum of $Num1 and $Num2 is $SUM" 
 
 # Ex: Num1=100 Num2=200 
 #output:sum of 100 and 200 is 300
