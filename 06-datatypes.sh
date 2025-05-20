a44e395fdf7f6ecb9b244c32803c55e5d944dd16
anjuma <anjuma.sk11@gmail.com>
2025-05-20 09:56:34 +0530

    datatypes

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -1,5 +1,11 @@
-Num1=$1
+Num1=$1 
 Num2=$2
 
 SUM=$(($Num1+$Num2))
-echo "sum of two $Num1 and $Num2 is $sum"
+$SUM
+echo "sum of $Num1 and $Num2 is $sum" 
+
+# Ex: Num1=100 Num2=200 
+#output:sum of 100 and 200 is 300
+# Ex: Num1= 100 Num2=anju 
+#output:sum of 100 and anju is 100
