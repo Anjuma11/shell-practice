baa703da2cd19cbd842b315244fd893aec12f927
anjuma <anjuma.sk11@gmail.com>
2025-06-05 10:49:30 +0530

    deleting old files

--- backup.sh
+++ backup.sh
@@ -1,91 +1,62 @@
+#!/bin/bash
+
+USERID=$(id -u)
+SOURCE_DIR=$1
+DESTI_DIR=$2
+DAYS=$(3:-14)
+
 R="\e[31m"
-G="\e[32m"
+G=="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-User=$(id -u)
-SOURCE_DIR=$1
-DESTI_DIR=$2
-DAYS=${3:-14}
-
+LOGS_FOLDER="/var/log/shell-script-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGS_FOLDER="/var/log/app-logs"
-LOG_FILE="$LOG_FOLDER/backup.log"
-SCRIPT_NAME=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SOURCE_DIR=/home/ec2-user/app-logs
+
+mkdir -p $LOGS_FOLDER
 
 check_root(){
-
-    if [ $User -eq 0 ]
-    then
-        echo "You are running with root access"
-    else
-        echo "ERROR :: Please run the script with root access"
+    if [ $USERID -ne 0 ]
+    then 
+        echo -e "$R ERROR :: $N Please run the script under root access"|tee -a $LOG_FILE
         exit 1
+    else
+        echo -e "You are running under root acccess" | tee -a $LOG_FILE
     fi
 }
 
 VALIDATE(){
     if [ $1 -eq 0 ]
-    then
-        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    then 
+        echo -e "$2 is..... $G success $N" | tee -a $LOG_FILE
     else
-        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        echo -e "$2 is..... $R failure $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
-check_root
-mkdir -p $LOGS_FOLDER
-
 USAGE(){
-
-	echo  -e " $R USAGE: : $N  sh.backup sh <Source directory> <Destination directory> <days>"
+    echo -e "$R USAGE:: $N backup.sh <source-dir> <desti-dir> <days(optionnal)>"
 }
+
+echo -e "Script started executing at $(date)" | tee -a $LOG_FILE
+
+FILES_TO_DELETE=$(find $1 -name "*.log" -mtime +$3)
 
 if [ $# -lt 2 ]
 then
     USAGE
 fi
 
-if [ ! -d $SOURCE_DIR ]
-then 
-	echo -e "$R Source directory $SOURCE_DIR doesn't exist $N"
-fi
+# if [ ! -d $2 ]
+# then
+#     echo -e ""
+# else
 
-if [ ! -d $DESTI_DIR ]
-then 
-	echo -e "$R destination directory $DESTI_DIR doesn't exist $N"
-fi
-
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-
-if [ ! -z $FILES_TO_DELETE ]
-then 
-	echo "Files to zip are : $FILES_TO_DELETE"
-	Timestamp=$(date +%F-%H-%M-%S)
-	Zip_file= "$DESTI_DIR/app-logs-$Timestamp.zip"
-	find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$Zip_file"
-	
-	if [ -f $Zip_file ]
-	then
-		echo -e "successfully created Zip file"
-
-    while IFS= read -r filepath
-    do
-        echo "deleting file: $filepath "
-        rm -rf $filepath
-    done <<< $FILES_TO_DELETE  
-
-    echo -e "log files older than $DAYS are deleted from $SOURCE_DIR "
-
-	else
-		echo -e "zip file creation…. $R Failure $N"
-
-        exit 1
-
-	fi
-else
-	echo -e "Files not found older than 14 days ....$Y SKIPPING $N"
-fi
-
-
+# if [ ! -d $1 ]
+# then
+#     echo -e ""
+# else
+# fi
